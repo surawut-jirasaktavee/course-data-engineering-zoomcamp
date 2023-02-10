01b8a1dedcb7febafff4440cccdcbcae56e61657
surawut.jirasaktavee <surawut.jirasaktavee@gmail.com>
2023-02-10 14:25:17 +0700

    update: prefect flow for fhv_tripdata 2019

---
+++ week_3/src/trip_data_2019/docker_deploy.py
@@ -0,0 +1,29 @@
+# type: ignore
+# pylint: disable=missing-module-docstring
+from etl_web_to_gcs import trip_data_2019_etl_parent_flow  # pylint: disable=import-error
+
+from prefect.deployments import Deployment  # pylint: disable=import-error
+from prefect.infrastructure.docker import DockerContainer # pylint: disable=import-error, ungrouped-imports
+from prefect.filesystems import GitHub  # pylint: disable=import-error
+
+github_block = GitHub.load("github-prefect-flow")
+docker_container_block = DockerContainer.load("fhv-tripdata-container-prefect-flow")
+
+docker_dep = Deployment.build_from_flow(
+    flow=trip_data_2019_etl_parent_flow,
+    name="trip_data_2019-wh-docker-flow",
+    version="1",
+    description="load data from web and save it to gcs",
+    tags=["docker-infra", "github-storage", "web-to-gcs"],
+    storage=github_block,
+    infrastructure=docker_container_block,
+    work_queue_name="default",
+    path="",
+    entrypoint="week_3/src/trip_data_2019/etl_web_to_gcs.py:trip_data_2019_etl_parent_flow",
+    parameters={"months": 12, "year": 2019, "dataset_name": "fhv_tripdata"},
+    output="./week_3/src/trip_data_2019/prefect-docker-deployment-web-to-gcs-trip_data2019.yaml",
+)
+
+
+if __name__ == "__main__":
+    docker_dep.apply()
---
+++ week_3/src/trip_data_2019/etl_web_to_gcs.py
@@ -0,0 +1,105 @@
+# type: ignore
+# pylint: disable=missing-module-docstring
+from pathlib import Path
+from datetime import timedelta  # pylint: disable=import-error
+
+import pandas as pd
+
+from prefect import flow, task  # pylint: disable=import-error
+from prefect_gcp.cloud_storage import GcsBucket  # pylint: disable=import-error
+# from prefect.tasks import task_input_hash # pylint: disable=ungrouped-imports, import-error
+
+
+@task(retries=3) #, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1)
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into pandas DataFrame"""
+
+    try:
+        print(f"Start download file from the given url: {dataset_url}")
+        df = pd.read_csv(dataset_url)  # pylint: disable=invalid-name
+        return df
+    except Exception as e: # pylint: disable=invalid-name
+        print(f"something went wrong with: {e}") # pylint: disable=invalid-name
+
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:  # pylint: disable=invalid-name
+    """Fix dtype issues"""
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+    return df
+
+
+@task()
+def write_local(
+    df: pd.DataFrame, color: str, dataset_file: str) -> Path:  # pylint: disable=invalid-name, redefined-outer-name)
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    data_dir = Path(f"data/{color}")
+    data_dir.mkdir(parents=True, exist_ok=True)
+    df.to_parquet(path, compression="gzip")
+    return path
+
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("gcs-taxi-trip-data", validate=False)
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+    return
+
+
+@flow()
+def etl_web_to_gcs(month: int, year: int, dataset_name: str="fhv_tripdata") -> None:  # pylint: disable=redefined-outer-name
+    """The main ETL function"""
+
+    dataset_file = f"{dataset_name}_{year}-{month}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/{dataset_file}.csv.gz"  # pylint: disable=line-too-long    
+   
+    df = fetch(dataset_url)  # pylint: disable=invalid-name
+    df_clean = clean(df)
+    path = write_local(df_clean, dataset_name, dataset_file)
+    write_gcs(path)
+
+
+@flow()
+def send_message():
+
+    """
+    Send message when the flow and sub task is complete
+    """
+
+    from prefect_slack import SlackWebhook  # pylint: disable=import-error, import-outside-toplevel
+    from prefect_slack.messages import send_incoming_webhook_message # pylint: disable=import-error, import-outside-toplevel
+
+    slack_webhook_block = SlackWebhook.load("slack-prefect-webhook-block")
+    send_incoming_webhook_message(
+        slack_webhook=slack_webhook_block,
+        text="Complete to download data from web and store in Google Cloud Storage",
+    )
+
+
+@flow()
+def trip_data_2019_etl_parent_flow(months: list[int], year: int, dataset_name: str):  # pylint: disable=redefined-outer-name
+
+    """
+    run parametize flow to main etl function
+    """
+    try:
+        for month in range(1, months + 1):  # pylint: disable=redefined-outer-name
+            etl_web_to_gcs(month, year, dataset_name)
+        send_message()
+    except: # pylint: disable=bare-except
+        print("Flow is not complete...")
+
+
+if __name__ == "__main__":
+
+    dataset_name = "fhv_tripdata"  # pylint: disable=invalid-name
+    year = 2019  # pylint: disable=invalid-name
+    months = 12 # pylint: disable=invalid-name
+  
+    trip_data_2019_etl_parent_flow(months, year, dataset_name)
